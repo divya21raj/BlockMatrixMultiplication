d64b8d3df81cabb1dc2efce6b93f1361e46fd22c
Divya Raj <divya21raj@gmail.com>
2017-11-27 13:28:42 +0530

    Initial commit

---
+++ src/com/d2runltd/LineMultiplier.java
@@ -0,0 +1,39 @@
+package com.d2runltd;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public class LineMultiplier implements Callable<int[][]>
+{
+	ArrayList<ArrayList<Integer>> A;
+	ArrayList<ArrayList<Integer>> B;
+	int start;
+	int end;
+	public int[][] C;
+
+	public LineMultiplier(ArrayList<ArrayList<Integer>> a,
+						  ArrayList<ArrayList<Integer>> b, int s, int e)
+	{
+		A = a;
+		B = b;
+		C = new int[a.size()][b.get(0).size()];
+		start = s;
+		end = e;
+	}
+
+	@Override
+	public int[][] call()
+	{
+		for (int i = start; i < end; i++)
+		{
+			for (int k = 0; k < B.size(); k++)
+			{
+				for (int j = 0; j < B.get(0).size(); j++)
+				{
+					C[i][j] += A.get(i).get(k) * B.get(k).get(j);
+				}
+			}
+		}
+		return C;
+	}
+}
---
+++ src/com/d2runltd/Main.java
@@ -0,0 +1,160 @@
+package com.d2runltd;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Main
+{
+    private static List<ArrayList<ArrayList<Integer>>> read(String filename)
+    {
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
+
+        String thisLine;
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            // Begin reading A
+            while ((thisLine = br.readLine()) != null)
+            {
+                if (thisLine.trim().equals(""))
+                    break;
+
+                else
+                {
+                    ArrayList<Integer> line = new ArrayList<>();
+                    String[] lineArray = thisLine.split("\t");
+
+                    for (String number : lineArray)
+                        line.add(Integer.parseInt(number));
+
+                    A.add(line);
+                }
+            }
+
+            // Begin reading B
+            while ((thisLine = br.readLine()) != null)
+            {
+                ArrayList<Integer> line = new ArrayList<>();
+                String[] lineArray = thisLine.split("\t");
+                for (String number : lineArray)
+                    line.add(Integer.parseInt(number));
+
+                B.add(line);
+            }
+
+            br.close();
+        } catch (IOException e)
+        {
+            System.err.println("Error: " + e);
+        }
+
+        List<ArrayList<ArrayList<Integer>>> res = new LinkedList<>();
+        res.add(A);
+        res.add(B);
+        return res;
+    }
+
+    private static void printMatrix(int[][] matrix)
+    {
+        for (int[] line : matrix)
+        {
+            int i = 0;
+            StringBuilder sb = new StringBuilder(matrix.length);
+            for (int number : line)
+            {
+                if (i != 0)
+                    sb.append("\t");
+                else
+                    i++;
+
+                sb.append(number);
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    private static int[][] parallelMult(ArrayList<ArrayList<Integer>> A,
+                                        ArrayList<ArrayList<Integer>> B, int threadNumber)
+    {
+        int[][] C = new int[A.size()][B.get(0).size()];
+        ExecutorService executor = Executors.newFixedThreadPool(threadNumber);
+        List<Future<int[][]>> list = new ArrayList<>();
+
+        int part = A.size() / threadNumber;
+        if (part < 1)
+            part = 1;
+
+        for (int i = 0; i < A.size(); i += part)
+        {
+            System.err.println(i);
+            Callable<int[][]> worker = new LineMultiplier(A, B, i, i+part);
+            Future<int[][]> submit = executor.submit(worker);
+            list.add(submit);
+        }
+
+        // now retrieve the result
+        int start = 0;
+        int CF[][];
+        for (Future<int[][]> future : list)
+        {
+            try
+            {
+                CF = future.get();
+                for (int i=start; i < start+part; i += 1)
+                {
+                    C[i] = CF[i];
+                }
+            } catch (InterruptedException | ExecutionException e)
+            {
+                e.printStackTrace();
+            }
+            start+=part;
+        }
+        executor.shutdown();
+
+        return C;
+    }
+
+    public static void main(String[] args)
+    {
+        String filename;
+
+        long startTime = System.currentTimeMillis();
+
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.err.println("Number of cores:\t" + cores);
+
+        int threads;
+        if (args.length < 3)
+        {
+            filename = "files\\3.in";
+            threads = cores;
+        }
+        else
+        {
+            filename = args[1];
+            threads = Integer.parseInt(args[2]);
+        }
+
+        List<ArrayList<ArrayList<Integer>>> matrices = read(filename);
+
+        ArrayList<ArrayList<Integer>> A = matrices.get(0);
+        ArrayList<ArrayList<Integer>> B = matrices.get(1);
+
+        int[][] C = parallelMult(A, B, threads);
+
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+
+        printMatrix(C);
+
+        System.out.println("Time taken: " + totalTime + "ms");
+    }
+}
